0d48456935e8fa2c6f0e02f803fc0b1347e52c69
Diyashree Manna @2009 <diyashree.manna2009@gmail.com>
2026-08-11 21:50:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ name=st.text_input("Enter your name:")
 #Displaying a message when a button is clicked
 if st.button ("Submit"):
     st.write(f"Hello,{name}: Welcome to Streamlit.")
-    
-    st.text_input(f"Enter your command:")
+   
+#Taking user input
+st.text_input(f"Enter your command:")
 #Displaying a message when a button is clicked
 if st.button ("Submit"):   
     st.write(f" I'm Fine ")
