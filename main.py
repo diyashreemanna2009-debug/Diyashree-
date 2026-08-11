8ee0e6d3aaadbea1631a69cf0111e128e7792371
Diyashree Manna @2009 <diyashree.manna2009@gmail.com>
2026-08-11 21:17:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ st.title("Interactive Streamlit App")
 name=st.text_input("Enter your name:")
 
 #Displaying a message when a button is clicked
-    if st.button ("Submit"):
-      st.write(f"Hello,{name}: Welcome to Streamlit,")
+if st.button ("Submit"):
+    st.write(f"Hello,{name}: Welcome to Streamlit,")
       
