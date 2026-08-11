42245ac96f7ab9fea0af9fb7f90083b903f1799d
Diyashree Manna @2009 <diyashree.manna2009@gmail.com>
2026-08-11 21:47:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ name=st.text_input("Enter your name:")
 if st.button ("Submit"):
     st.write(f"Hello,{name}: Welcome to Streamlit.")
     st.text_input(f"Enter yuor command:")
-    st.write(f" How are you?")
+ if st.button ("Submit"):   
+    st.write(f" I'm Fine ")
     
